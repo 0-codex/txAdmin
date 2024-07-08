712262fc028b020bebdb7b17d8d822303552a40b
ordi2322930 <ordi2322930@educ.normandie.fr>
2024-07-09 00:04:22 +0200

    feat: start creation of the page report gestion

--- nui/src/components/ReportsModal/ReportAdminModal.tsx
+++ nui/src/components/ReportsModal/ReportAdminModal.tsx
@@ -1,8 +1,48 @@
-import {Box, DialogContent, DialogTitle, IconButton, Typography} from "@mui/material";
-import {BugReport, Close, DirectionsWalk, QuestionMark, ReportProblem} from "@mui/icons-material";
+import {
+    Box,
+    DialogContent,
+    DialogTitle,
+    IconButton, List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography
+} from "@mui/material";
+import {
+    Article,
+    BugReport,
+    Close, Details,
+    DirectionsWalk,
+    FlashOn,
+    HealthAndSafety,
+    QuestionMark, Radar,
+    ReportProblem
+} from "@mui/icons-material";
 import React from "react";
 import {Report, ReportType} from "@nui/src/state/reports.state";
 import {useReportModal} from "@nui/src/provider/ReportModalProvider";
+
+const tabs: {
+    label: string,
+    icon: React.ReactNode,
+    menu: React.ReactNode
+}[] = [
+    {
+        label: "Action",
+        icon: <FlashOn/>,
+        menu: <Box></Box>
+    },
+    {
+        label: "Info",
+        icon: <Article/>,
+        menu: <Box></Box>
+    },
+    {
+        label: "Players Proximity",
+        icon: <Radar/>,
+        menu: <Box></Box>
+    }
+]
 
 export default function ReportAdminModal({data}: {data: Report | null}) {
     const reportModal = useReportModal()
@@ -38,8 +78,26 @@ export default function ReportAdminModal({data}: {data: Report | null}) {
                 </DialogTitle>
                 <IconButton onClick={closeHandler}><Close/></IconButton>
             </Box>
-            <DialogContent>
-                <Typography>{data?.type}</Typography>
+            <DialogContent sx={{
+                display: "flex"
+            }}>
+                <List>
+                    {
+                        tabs.map(tab => (
+                            <ListItemButton key={tab.label.toLowerCase().replace(" ", "_")} sx={{
+                                borderRadius: 2
+                            }}>
+                                <ListItemIcon>{tab.icon}</ListItemIcon>
+                                <ListItemText>{tab.label}</ListItemText>
+                            </ListItemButton>
+                        ))
+                    }
+                </List>
+                <Box sx={{
+                    flex: 1
+                }}>
+
+                </Box>
             </DialogContent>
         </>
     )
